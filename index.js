8c82082a5d535beb8a398f13747a82169926d242
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2023-01-01 16:31:59 +0700

    learn js

---
+++ index.js
@@ -0,0 +1,23 @@
+//if else
+// const x = 22;
+// const y = 23;
+
+// if (x <= y) {
+//   console.log("salah");
+// } else {
+//   console.log("benar");
+// }
+
+//loop for
+// let i;
+
+// for (i = 1; i < 10; i++) {
+//   console.log("test");
+// }
+
+//loop do while
+let i = 1;
+do {
+  console.log("testtt");
+  i = i + 1;
+} while (i <= 10);
